af954d3a8493919223bcc98c2a6f48ef2af61dc3
Pixwok <40206880+Pixwok@users.noreply.github.com>
2020-05-26 08:53:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 HOST = "irc.chat.twitch.tv"
-PORT = 6667
-PASS = "oauth:0xslg7lg0tutesf4lh4yh4yh8cnahi"
-NICK = "PixwokBot"
+PORT = YOUR_PORT
+PASS = "YOUR_OAUTH_API_TWITCH"
+NICK = "BOT_NAME"
 CHANNEL = "#cappixwok"
